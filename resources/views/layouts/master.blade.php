d2de4a3603f9706158848cd268033176bd7b9088
babacar44 <babacamara91.com>
2020-01-13 21:06:21 +0000

    color awesome

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -130,10 +130,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </li>
             {{-- logout --}}
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('logout') }}"
+                <a class="nav-link"  href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
-                           <i class="nav-icon fas fa-power-off"></i>
+                           <i class="nav-icon fas fa-power-off red"></i>
                         <p>
                                
                                 {{ __('Logout') }}
